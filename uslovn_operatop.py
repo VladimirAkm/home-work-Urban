7a7c8fb2ade7a3df8dcbe1abf9168ee45ed2f078
VladimirAkm <akmvova@yndex.ru>
2024-01-19 23:22:17 +0300

    data

--- uslovn_operatop.py
+++ uslovn_operatop.py
@@ -7,13 +7,10 @@ print('дотвидания!')
 a, b = 10, 5
 if a > b:
     print('a > b')
-
 if a > b and a > 0:
     print('успех')
-
 if a > b and (a > 0 or b < 1000):
     print('успех')
-
 if 5 < b < 10:
     print('успех')
 
