d8dd6a6206aab99b19d03e16402acb167a63cff4
VladimirAkm <akmvova@yndex.ru>
2024-03-06 07:24:08 +0300

    наследование классов

--- inheritance_1.py
+++ inheritance_1.py
@@ -5,18 +5,18 @@ class Car:
         self.horse_powers = 0
 
     def __str__(self):
-        return '{}, {} стоимость,{} мощность'.format(self.__class__.__name__, self.Price, self.horse_powers)
+        return '{}: стоимость- {}, мощность- {} '.format(self.__class__.__name__, self.Price, self.horse_powers)
 
 
 class Nissan(Car):
-
     def horse_powers(self):
+    Price = 1500000
         self.horse_powers = 2000
 
 
 class Kia(Car):
-
     def horse_powers(self):
+    Price = 1200000
         self.horse_powers = 1600
 
 
